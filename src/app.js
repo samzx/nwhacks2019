7c435b47cc3fadb3e8f144f384154b0c54ae65d4
Sam Xie <samxie@outlook.com.au>
2019-01-26 14:12:21 -0800

    send bundle

--- src/app.js
+++ src/app.js
@@ -4,11 +4,16 @@ import ReactDOM from "react-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
+import Menu from './components/Menu';
+import Map from './components/Map';
+
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
+        <Map/>
+        <Menu/>
       </div>
     );
   }
---
+++ src/components/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Menu = (props) => (
+  <div>
+    Hi I'm a menu
+  </div>
+)
+
+export default Menu;
